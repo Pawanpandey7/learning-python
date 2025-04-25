090acccbebcbe0b4da853687c74e36f484afe927
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-04-25 22:41:40 +0545

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+#Stores your name, age, and hobby in variables.
+#Prints them all with proper labels.
+#Uses type() to print the data type of each variable.
+
+name = "Pawan pandey"
+age = 23
+hobby = "swimming"
+print("my name is ",name)
+print("i am "+str(age)+" years old.")
+print("I love ", hobby)
+print("the datatype of the name variable is",type(name))
+print("the datatype of the age varibale is ",type(age))
+print("the datatype of the hobby variable is ",type(hobby))
+

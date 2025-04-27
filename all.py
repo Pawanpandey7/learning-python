6eb29534f816fa309af5fef7593c76f5cc946972
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-04-27 21:59:46 +0545

    understaning the difference between list and tuple

---
+++ all.py
@@ -0,0 +1,17 @@
+# here i will show the difference between the list and the tuple
+
+list = [1,2,3,4,5] # it is the list. we can modify it
+
+tup = (1,2,3,4,5) # this is tuple we cannot make changes to it
+
+# one of the feature of the tuple is that we can insert tuple within a tuple
+tup2 =(1,2,(3,4),5)
+
+# also we can define tuple like this:
+
+tup3 = 1, 2, 3
+
+#let me see if i can access the elements of the tuple
+
+print(tup[0])
+print(tup2[2][0])

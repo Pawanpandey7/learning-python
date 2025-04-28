dc8aaee33ddf500d5428cad28eac3452951bcb5b
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-04-28 21:26:53 +0545

    list,tuple,dictionary and sets in python

--- all.py
+++ all.py
@@ -15,3 +15,34 @@ tup3 = 1, 2, 3
 
 print(tup[0])
 print(tup2[2][0])
+
+#dictionary
+
+dict = {
+    "name": "Pawan pandey",
+    "age": 23,
+    "hobby": "coding"
+}
+# we can add data to the dictionary by 
+dict["course"] = "BIT" 
+
+# we can modify the dictionary by 
+dict["hobby"]="singing"
+
+#we can delete the data from the dictionary by 
+del dict["age"]
+
+# set
+
+#it is the unordered data structure
+# we cannot index it like list and tuple
+# there should be no repetition
+#it can be modified
+
+set1 = {1,2,2,3,4,5}
+print(set) #output {1,2,3,4,5}
+
+# we cannot access a element of the set by using set1[0] like the list and tuple
+
+
+
